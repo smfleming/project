d32e40629bb4403a3142b3129c6fe4ff412cccbe
Steve Fleming <stephen.fleming@ucl.ac.uk>
2022-08-31 12:44:41 +0000

    Initial commit

---
+++ git_load.R
@@ -0,0 +1,6 @@
+# use a function without loading the package
+# package::function
+usethis::use_git_config(
+  user.name = "Steve Fleming",
+  user.email = "stephen.fleming@ucl.ac.uk",
+  init.defaultBranch = "main") # <-- not necessary but kinder than 'master'
